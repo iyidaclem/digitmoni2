1e28f906d37ca0a71de34061c1e7b102dd6807a4
Ikechukwu-Unegbu <42593643+Ikechukwu-Unegbu@users.noreply.github.com>
2021-06-19 10:35:52 +0100

    'progress'

---
+++ App/test/DBTest.php
@@ -0,0 +1,21 @@
+<?php 
+
+require_once '/database/DataBase.php';
+require_once '../core/Response.php';
+require_once '../helpers/debugHelper.php';
+
+
+// function insert(){
+//   $db = DataBase::getInstance();
+//   $username = 'Ikechukwu';
+//   $access_token = 'akjfka9r0392rfnvafilaefola';
+//   $user_agent = 'Browser, firefox- HP Probook';
+//   $token_exp = '2021-09-09';
+//   $fields = ['username'=>$username,	'access_token'=>$access_token, 	'user_agent'=>$user_agent,	'token_exp'=>$token_exp];
+
+//   $db->query("INSERT INTER session_tb WHERE username=? 
+//   and access_token=? and user_agent=? and token_exp=?", $fields);
+//   $db->insert('session_tb', $fields);
+// }
+
+// insert();
